776f595b1b91ec8f940efd65ec161e11631c3884
Lizzie Matusov <eamatusov@gmail.com>
2021-01-13 14:39:36 -0500

    Allow for remote bluetooth connections between 2 various arduinos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import  WriteValue from './WriteValue/WriteValue.js';
+import Slider, { Range } from 'rc-slider';
 import './App.css';
+import WriteValue from './WriteValue/WriteValue';
 
 function App() {
   const [supportsBluetooth, setSupportsBluetooth] = useState(false);
   const [isDisconnected, setIsDisconnected] = useState(true);
   const [deviceName, setDeviceName] = useState(null);
   const [hexValue, setHexValue] = useState(0);
+  const [motorCharacteristicService, setMotorCharacteristicService] = useState(null);
+
+  const SEND_SERVICE_LIZZIE = "251fb674-1665-40e4-8bf0-24a97efba116";
+  const SEND_SERVICE_CHARACTERISTIC_LIZZIE = "6e260f41-5fee-424a-879d-0e28aa8b7136";
+  const DEVICE_NAME_LIZZIE = "Wifi Arduino Friend";
+  
+  const SEND_SERVICE_TOSIN = "251fb675-1665-40e4-8bf0-24a97efba116"
+  const SEND_SERVICE_CHARACTERISTIC_TOSIN = "6e260f41-5fee-424a-879e-0e28aa8b7136"
+  const DEVICE_NAME_TOSIN = "Tosins Soln Code NANO BLE 33 SENSE"
+
+  const bytesArray = (n) => {
+    if (!n) return new ArrayBuffer(0)
+    const a = []
+    a.unshift(n & 255)
+    while (n >= 256) {
+      n = n >>> 8
+      a.unshift(n & 255)
+    }
+    return new Uint8Array(a).buffer
+  }
 
   // When the component mounts, check that the browser supports Bluetooth
   useEffect(() => {
@@ -31,19 +52,30 @@ function App() {
     try {
       // Search for Bluetooth device to connect to the browser
       const device = await navigator.bluetooth
+        // .requestDevice({
+        //   acceptAllDevices: true
+        // });
       .requestDevice({
-        acceptAllDevices: true
-      });
+      acceptAllDevices: true,
+      optionalServices: [SEND_SERVICE_TOSIN]
+    });
 
       setDeviceName(device.name);
       setIsDisconnected(false);
 
       // Add an event listener to detect when a device disconnects
       device.addEventListener('gattserverdisconnected', onDisconnected);
-      console.log(device.name)
 
       // Try to connect to the remote GATT Server running on the Bluetooth device
-      const server = await device.gatt.connect();
+      const server = await device.gatt.connect()
+
+      const primaryService = await server.getPrimaryService(SEND_SERVICE_TOSIN);
+      console.log(primaryService)
+      
+      const motorCharacteristic = await primaryService.getCharacteristic(SEND_SERVICE_CHARACTERISTIC_TOSIN);
+      console.log(motorCharacteristic);
+      setMotorCharacteristicService(motorCharacteristic);
+      
       
     } catch(error) {
       console.log(`There was an error: ${error}`);
@@ -51,7 +83,9 @@ function App() {
   };
 
 const handleSubmit = async () => {
-  console.log('hi')
+  console.log(motorCharacteristicService)
+  const reading = await motorCharacteristicService.writeValue(bytesArray(50));
+
 }
 
 // this is broken!
@@ -63,12 +97,8 @@ const handleChange =(event) => {
     <div className="App">
       <h1>Hello! Send a virtual pulse hug to a friend :-)</h1>
       {supportsBluetooth && !isDisconnected && 
-            <form onSubmit={handleSubmit}>
-            <label>Enter Value: 
-                <input type="text" value={hexValue} onChange={handleChange}/>
-                <input type="submit" value="Submit" />
-            </label>
-        </form>
+      <WriteValue characteristic={motorCharacteristicService}></WriteValue>
+        //<button onClick={handleSubmit}>Send pulse</button>
       
       }
       {supportsBluetooth && isDisconnected &&
--- src/WriteValue/WriteValue.js
+++ src/WriteValue/WriteValue.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import Slider, { Range } from 'rc-slider';
+
 
 class WriteValue extends React.Component {
-    constructor() {
+    constructor(props) {
+        super(props);
         this.state = {value: ''};
-    }
-    writeValue() {
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+
+    bytesArray = (n) => {
+        if (!n) return new ArrayBuffer(0)
+        const a = []
+        a.unshift(n & 255)
+        while (n >= 256) {
+          n = n >>> 8
+          a.unshift(n & 255)
+        }
+        return new Uint8Array(a).buffer
+      }
+
+    handleSubmit(event) {
+        this.props.characteristic.writeValue(this.bytesArray(this.state.value))
+        event.preventDefault();
+    }
+
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
             <label>Enter Value: 
-                <input type="text" value={this.state.value} />
+                <input type="text" value={this.state.value} onChange={this.handleChange}/>
                 <input type="submit" value="Submit" />
             </label>
         </form>
+        //   <Slider color="red"/>
+    
       );
     }
   }
